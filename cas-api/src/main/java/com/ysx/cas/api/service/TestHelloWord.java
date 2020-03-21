d3a53acbeda8e50a95fb58c093813198f9970cdf
lichaochao <lichaochao@yunshuxie.com>
2020-03-21 14:17:02 +0800

    cas基础

---
+++ cas-api/src/main/java/com/ysx/cas/api/service/TestHelloWord.java
@@ -0,0 +1,11 @@
+package com.ysx.cas.api.service;
+
+
+import java.util.List;
+
+/**
+ *
+ */
+public interface TestHelloWord {
+    public String getHelloWord();
+}
---
+++ cas-api/src/test/java/com/ysx/cas/api/CrmApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ysx.cas.api;
+
+
+
+class CrmApiApplicationTests {
+
+    /**
+     * Rigorous Test :-)
+     */
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+
+}
